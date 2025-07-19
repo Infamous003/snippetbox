513724e69fb44845715ba1553f8cd2c4b232fc72
Infamous003 <arfathsyed0312@gmail.com>
2025-07-19 20:15:54 +0530

    Add basic routes and a route handler

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Home page"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating a snipper"))
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Starting server on port 4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
