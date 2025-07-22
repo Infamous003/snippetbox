627a753cb438c1f9154c3a7f7431943313d70928
Infamous003 <arfathsyed0312@gmail.com>
2025-07-22 17:34:29 +0530

    Add logs, srv type

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// We are defining a command line argument for port
+	// using the string function. it converts anything that you enter to a string, if it can that is
+	// the string func retuns the address of the addr variable that line sin the runtime
+	// default value is 4000
+	addr := flag.String("addr", ":4000", "Port number")
+
+	// Then we need to parse it
+	flag.Parse()
+
+	// Creating a logger for informational messages and errors
+	// the last argument uses bitwise or to sombine date and time of the log
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -16,7 +33,15 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting server on port 4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on port %s", *addr)
+
+	// Sending the pointer address. deferencing it basiclaly
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
