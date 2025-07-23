f46dccc637f0cd8bab0f4da0782404181a30679b
Infamous003 <arfathsyed0312@gmail.com>
2025-07-23 20:52:02 +0530

    Add postgreSQL connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 type application struct {
@@ -12,18 +16,10 @@ type application struct {
 	infoLog  *log.Logger
 }
 
+const DATABASE_URL = "postgres://infamous:Getalife@03@localhost:5432/snippetbox"
+
 func main() {
-	// We are defining a command line argument for port
-	// using the string function. it converts anything that you enter to a string, if it can that is
-	// the string func retuns the address of the addr variable that line sin the runtime
-	// default value is 4000
-	addr := flag.String("addr", ":4000", "Port number")
-
-	// Then we need to parse it
-	flag.Parse()
-
 	// Creating a logger for informational messages and errors
-	// the last argument uses bitwise or to sombine date and time of the log
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
@@ -31,6 +27,10 @@ func main() {
 		infoLog:  infoLog,
 		errorLog: errorLog,
 	}
+	// Creating a command line argument/flag for port number
+	addr := flag.String("addr", ":4000", "Port number")
+	// Then we need to parse it
+	flag.Parse()
 
 	srv := &http.Server{
 		Addr:     *addr,
@@ -38,9 +38,36 @@ func main() {
 		Handler:  app.routes(),
 	}
 
+	app.infoLog.Println("Connecting to database...")
+	conn, err := openDB(DATABASE_URL)
+	if err != nil {
+		app.errorLog.Printf("Unable to connect to database: %v\n", err.Error())
+	}
+	app.infoLog.Println("Database connection established!")
+
+	defer conn.Close(context.Background())
+
+	output, err := conn.Exec(context.Background(), "SELECT * FROM snippets")
+	if err != nil {
+		app.errorLog.Println(err.Error())
+	}
+	fmt.Println(output)
+
 	infoLog.Printf("Starting server on port %s", *addr)
 
 	// Sending the pointer address. deferencing it basiclaly
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(databaseUrl string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = conn.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
